9fefb947b430597cc1b6dca3e09d6777d7bc2578
Catarina Matos <Catarina.Barbosa.Matos@formandos.cinel.pt>
2021-01-12 16:09:57 +0000

    Criar a base de dados e tabela de produtos

---
+++ Loja-Cet50/Dados/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Loja_Cet50.Dados.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_Cet50.Dados
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Produto> Produtos { get; set; }
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Loja-Cet50/Dados/Entidades/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_Cet50.Dados.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false )]
+        public decimal Preco { get; set; }
+
+        [Display(Name ="Image")]
+        public string UrlDaImagem { get; set; }
+
+        [Display(Name = "Última Compra")]
+        public DateTime UltimaCompra { get; set; }
+
+        [Display(Name = "Última Venda")]
+        public DateTime UltimaVenda { get; set; }
+
+        [Display(Name = "Disponível")]
+        public bool Disponivel { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+    }
+}
